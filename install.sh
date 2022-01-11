3f8d0bfe04fdf77640e81e657463384e4202b2e1
Niyabi <kufel92@gmail.com>
2022-01-11 10:39:45 +0100

    Bugfix - not able to install magento

--- install.sh
+++ install.sh
@@ -11,50 +11,15 @@ if [ -z "$(ls -A $WORKDIR)" ]; then
 
   lando start
   lando xdebug-off
-  lando composer self-update
   cd ./docroot
 
   echo "Creating magento project"
-  lando composer --no-plugins config --global http-basic.repo.magento.com $PUBLIC_KEY $PRIVATE_KEY
-  lando composer --no-plugins create-project --no-install --repository-url=https://repo.magento.com/ magento/project-community-edition .
-
-  echo "Installing magento dependencies"
-  lando composer --no-plugins config http-basic.repo.magento.com $PUBLIC_KEY $PRIVATE_KEY
-  lando composer --no-plugins install --no-interaction --ignore-platform-reqs
+  lando composer config --global http-basic.repo.magento.com $PUBLIC_KEY $PRIVATE_KEY
+  lando composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition .
+  lando composer config http-basic.repo.magento.com $PUBLIC_KEY $PRIVATE_KEY
 
   echo "Install magento"
-  lando ssh -c 'bin/magento setup:install \
-    --db-host=database \
-    --db-name=magento \
-    --db-user=magento \
-    --db-password=magento \
-    --language=en_US \
-    --currency=USD \
-    --timezone=Europe/Warsaw \
-    --use-rewrites=1 \
-    --session-save=redis \
-    --session-save-redis-host=redis \
-    --session-save-redis-port=6379 \
-    --session-save-redis-db=0 \
-    --session-save-redis-disable-locking=1 \
-    --cache-backend=redis \
-    --cache-backend-redis-server=redis \
-    --cache-backend-redis-db=0 \
-    --cache-backend-redis-port=6379 \
-    --page-cache=redis \
-    --page-cache-redis-server=redis \
-    --page-cache-redis-db=1 \
-    --page-cache-redis-port=6379 \
-    --backend-frontname="admin" \
-    --search-engine=elasticsearch7 \
-    --elasticsearch-host=elasticsearch \
-    --elasticsearch-port=9200 \
-    --elasticsearch-index-prefix magento \
-    --admin-firstname=Magento \
-    --admin-lastname=User \
-    --admin-email=user@example.com \
-    --admin-user=admin \
-    --admin-password=admin123'
+  lando ssh -c 'bin/magento setup:install --db-host=database --db-name=magento --db-user=magento --db-password=magento --language=en_US --currency=USD --timezone=Europe/Warsaw --use-rewrites=1 --session-save=redis --session-save-redis-host=redis --session-save-redis-port=6379 --session-save-redis-db=0 --session-save-redis-disable-locking=1 --cache-backend=redis --cache-backend-redis-server=redis --cache-backend-redis-db=0 --cache-backend-redis-port=6379 --page-cache=redis --page-cache-redis-server=redis --page-cache-redis-db=1 --page-cache-redis-port=6379 --backend-frontname=admin --search-engine=elasticsearch7 --elasticsearch-host=elasticsearch --elasticsearch-port=9200 --elasticsearch-index-prefix magento --admin-firstname=Magento --admin-lastname=User --admin-email=user@example.com --admin-user=admin --admin-password=admin123'
 
   lando ssh -c "bin/magento config:set web/secure/base_url https://magento2.lndo.site/"
   lando ssh -c "bin/magento config:set web/unsecure/base_url https://magento2.lndo.site/"
